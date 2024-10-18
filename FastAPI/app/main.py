aeb33e2c85e4dbd88066bb35666b1b1959308e06
Bastien GUILLOU <bastien.guillou@edu.esiee.fr>
2024-10-18 14:44:22 +0200

    Affichage pokemon DB

--- FastAPI/app/main.py
+++ FastAPI/app/main.py
@@ -1,16 +1,16 @@
-from fastapi import FastAPI, Depends, Request, Response
+from fastapi import FastAPI, Depends, Request, Response, status
 from sqlalchemy.orm import Session
 from sqlalchemy import func
 from database import SessionLocal
-from models import PokemonType, Type
+from models import Pokemon, PokemonType, Type
 from fastapi.templating import Jinja2Templates
 import matplotlib.pyplot as plt
 from io import BytesIO
+from sqlalchemy.orm import joinedload
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-# Dependency to get the database session
 def get_db():
     db = SessionLocal()
     try:
@@ -24,9 +24,10 @@ def read_root(request: Request):
 
 @app.get("/pokemon/type_count", response_class=Response)
 def read_type_distribution(db: Session = Depends(get_db)):
-    type_distribution = db.query(Type.type_nom, func.count(PokemonType.numero).label('count'))\
-        .join(Type, PokemonType.type_id == Type.type_id)\
-        .group_by(Type.type_nom).all()
+    type_distribution = db.query(
+        Type.type_nom, func.count(PokemonType.numero).label('count')
+    ).join(Type, PokemonType.type_id == Type.type_id)\
+     .group_by(Type.type_nom).all()
 
     # Data for plotting
     type_names = [type_name for type_name, _ in type_distribution]
@@ -47,3 +48,34 @@ def read_type_distribution(db: Session = Depends(get_db)):
     buf.seek(0)
 
     return Response(content=buf.getvalue(), media_type="image/png")
+
+@app.get("/pokemon/view_db")
+def view_db(request: Request, db: Session = Depends(get_db)):
+    all_pokemon = db.query(
+        Pokemon.nom,
+        Pokemon.image,
+        Type.type_nom
+    ).select_from(Pokemon)\
+     .join(PokemonType, PokemonType.numero == Pokemon.numero)\
+     .join(Type, PokemonType.type_id == Type.type_id)\
+     .order_by(Pokemon.nom).all()
+
+    pokemon_data = []
+    for pokemon in all_pokemon:
+        if not any(p['nom'] == pokemon.nom for p in pokemon_data):
+            pokemon_data.append({
+                "nom": pokemon.nom,
+                "types": [pokemon.type_nom],
+                "image": pokemon.image
+            })
+        else:
+            next(p for p in pokemon_data if p['nom'] == pokemon.nom)['types'].append(pokemon.type_nom)
+
+    return templates.TemplateResponse("view_db.html", {
+        "request": request,
+        "pokemon_data": pokemon_data
+    })
+
+
+
+
--- FastAPI/app/models.py
+++ FastAPI/app/models.py
@@ -4,18 +4,25 @@ from database import Base
 
 class Pokemon(Base):
     __tablename__ = 'pokemon'
-
     numero = Column(Integer, primary_key=True)
     nom = Column(String)
+    image = Column(String)
+    types = relationship("Type", secondary="pokemon_type", back_populates="pokemons")
 
 class Type(Base):
     __tablename__ = 'type'
-
-    type_id = Column(Integer, primary_key=True, autoincrement=True)
+    type_id = Column(Integer, primary_key=True)
     type_nom = Column(String, unique=True)
+    pokemons = relationship("Pokemon", secondary="pokemon_type", back_populates="types")
+
 
 class PokemonType(Base):
     __tablename__ = 'pokemon_type'
-
     numero = Column(Integer, ForeignKey('pokemon.numero'), primary_key=True)
     type_id = Column(Integer, ForeignKey('type.type_id'), primary_key=True)
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
